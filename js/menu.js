3427cd757ce5548dba39f0c55c0faef57233f9a8
RavelloH <hyh20060327@qq.com>
2022-09-12 10:09:26 +0800

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -6,5 +6,4 @@ document.writeln("<li><a href=\'/\'><span class=\'iconfontsmall icon-tag\'></spa
 document.writeln("<li><a href=\'/\'><span class=\'iconfontsmall icon-classification\'></span> 自定义</a>");
 document.writeln("<li><a href=\'/\'><span class=\'iconfontsmall icon-friend\'></span> 自定义</a></li>");
 document.writeln("<li><a href=\'/\'><span class=\'iconfontsmall icon-about\'></span> 自定义</a></li>");
-document.writeln("<b><br><br>Copyright © 2019 - <b id=\'year\'></b> User.<br> All rights reserved.</b><br>");
-document.writeln("Theme by <a href=\'https://ravelloh.github.io\'>RavelloH</a>:<a href=\'https://github.com/RavelloH/RTheme\' class=\'linkline\'>RTheme</a>");
+document.writeln("<b><br><br>Copyright © 2021 - <b id=\'year\'></b> Brossas Zaichek.<br> All rights reserved.</b><br>");
